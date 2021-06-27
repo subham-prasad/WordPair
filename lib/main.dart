3581f34e8839e38ec5611417bba17fa471ecf14f
subham-prasad <shubhamprasad2000@gmail.com>
2021-06-28 00:54:27 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp(
+    theme: ThemeData(primaryColor: Colors.red[700]),
+    home: Scaffold(
+          appBar: AppBar(title: Text('WordPair Generator')),
+          body: Center(child: Text(wordPair.asPascalCase))));
+} 
+}
